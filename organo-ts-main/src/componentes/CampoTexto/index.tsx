c29592d75dfdcec5568d374cc3ad425d5730a131
Lucas Martins <lucasdallmartims@gmail.com>
2023-10-30 16:45:01 -0300

    first commit

---
+++ organo-ts-main/src/componentes/CampoTexto/index.tsx
@@ -0,0 +1,40 @@
+import "./CampoTexto.css";
+
+interface CampoTextoProps {
+  aoAlterado: (valor: string) => void;
+  placeholder: string;
+  label: string;
+  valor: string;
+  obrigatorio?: boolean;
+  tipo?: "text" | "password" | "date" | "email" | "number";
+}
+
+const CampoTexto = ({
+  aoAlterado,
+  label,
+  obrigatorio = false,
+  valor,
+  placeholder,
+  tipo = "text",
+}: CampoTextoProps) => {
+  const placeholderModificada = `${placeholder}...`;
+
+  const aoDigitado = (evento: React.ChangeEvent<HTMLInputElement>) => {
+    aoAlterado(evento.target.value);
+  };
+
+  return (
+    <div className="campo-texto">
+      <label>{label}</label>
+      <input
+        value={valor}
+        onChange={(evento) => aoDigitado}
+        required={obrigatorio}
+        placeholder={placeholderModificada}
+        type={tipo}
+      />
+    </div>
+  );
+};
+
+export default CampoTexto;
